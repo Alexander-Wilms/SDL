9c8eb0f1bcfb3566b7fef8f1db0a9a8426ffbe0f
Alexander Wilms <f.alexander.wilms@gmail.com>
2017-01-15 12:50:28 +0100

    Display Grid

---
+++ Display Grid/src/main.cpp
@@ -0,0 +1,46 @@
+// https://wiki.libsdl.org/SDL_CreateRenderer
+
+#include <iostream>
+#include <SDL.h>
+#include "SDL_image.h"
+
+int main(int, char**){
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Window title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_SetRenderDrawColor(renderer,255,10,50,120);
+
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer,37,72,92,120);
+
+	SDL_Rect* rect;
+
+	rect->w = 1;
+	rect->h = 1;
+
+	for(int i = 0; i <500; i++) {
+		for(int j = 0; j <500; j++) {
+			rect->x = i;
+			rect->y = j;
+
+			SDL_RenderFillRect(renderer, rect);
+		}
+	}
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	SDL_DestroyRenderer(renderer);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
+}
